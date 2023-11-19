2d2d1e70e84d1623ab6eaaf3c2c288e7a2df3795
futurama88 <147860808+futurama88@users.noreply.github.com>
2023-11-19 20:02:29 +0300

    Add files via upload
    Skillfactory Итоговый проект 5.6 (PJ-02)

---
+++ confing.py
@@ -0,0 +1,11 @@
+
+
+
+TOKEN = '6341126346:AAFCDUScyVHodSvSfYvn9z2PM2rcI4Cwbus'
+
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
---
+++ extensions.py
@@ -0,0 +1,39 @@
+import requests
+import json
+from confing import keys
+
+
+
+
+
+
+class ConvertionException(Exception):
+    pass
+
+
+class CryptoConverter:
+    @staticmethod
+    def get_price (quote: str, base: str, amount: str):
+
+        if quote == base:
+            raise ConvertionException(f'Невозможно перевисти одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не существующая волюта валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не существующая волюта валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f'Не правильное количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+
+        return  total_base
